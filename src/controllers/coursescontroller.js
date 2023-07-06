960eb4501fe908c53c9638da542a3e718732d153
woaitsAryan <yoprince.ab@gmail.com>
2023-07-06 19:48:11 +0530

    small fixes

--- src/controllers/coursescontroller.js
+++ src/controllers/coursescontroller.js
@@ -1,10 +1,13 @@
-import catchAsync from "../helpers/catchAsync";
-import Course from "../models/courseModel";
+import catchAsync from "../helpers/catchAsync.js";
+import Course from "../models/courseModel.js";
 
 export const Coursecontroller = catchAsync(
     async (req, res) => {
         const section = req.params.section;
         const courses = await Course.findOne({ type: section });
+        if(!courses){
+            return res.status(400).json({ error: 'Invalid section' });
+        }
         return res.json({ coursesdata: courses.details });
 })
 
@@ -13,6 +16,9 @@ export const Slotcontroller = catchAsync(
         const section = req.params.section;
         const course = req.params.course;
         const courses = await Course.findOne({ type: section });
+        if(!courses){
+            return res.status(400).json({ error: 'Invalid section' });
+        }
         const slots = courses.details.filter((item) => item.name === course);
         return res.json({ slots: slots[0].slots });
     }
